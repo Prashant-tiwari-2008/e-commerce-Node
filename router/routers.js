0d58ad45ff1f5869ee2dff4105b49b8f8cc10988
Prashant tiwari <prashanttiwari.vns@gmail.com>
2022-11-07 13:16:48 +0530

    final commit

---
+++ router/routers.js
@@ -0,0 +1,21 @@
+const { userRouter } = require('./user-router');
+const { sizeRouter } = require('./size-router');
+const { tagRouter } = require('./tag-router');
+const { usertypeRouter } = require('./usertype-router');
+const { colorRouter } = require('./color-router');
+const brandlogoRouter = require('./brandlogo-router');
+const categoryRouter = require('./category-router');
+const productRouter = require('./product-router');
+const orderRouter = require('./order-router');
+
+module.exports = {
+    userRouter,
+    sizeRouter,
+    tagRouter,
+    usertypeRouter,
+    colorRouter,
+    brandlogoRouter,
+    categoryRouter,
+    productRouter,
+    orderRouter
+};
